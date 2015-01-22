3cc041913512023d85dee0da8c27f0af4374c366
kjgilbert <kgilbert@zoology.ubc.ca>
2015-01-21 22:00:46 -0800

    update R file to work here versus the VM

--- My_Exercises/Jan21_AlignmentExercises/MyRCommands_Aligning.R
+++ My_Exercises/Jan21_AlignmentExercises/MyRCommands_Aligning.R
@@ -1,6 +1,7 @@
 library(rbamtools)  #library for align tools?
 
-setwd("/home/darwin/Kim/Exercises_Day2/Alignments_Data")
+#setwd("/home/darwin/Kim/Exercises_Day2/Alignments_Data")
+setwd("~/Documents/My_Documents/UBC/Classes/BIOL525_Bioinformatics/Bio525D/My_Exercises/Jan21_AlignmentExercises")
 
 reader <- bamReader("cp_350PE_01Err_bowtie2_sorted.bam", idx=TRUE) # read in the sorted bam file from bowtie2
 getRefData(reader)  # gives the name of the sequence in the reference?
@@ -35,35 +36,35 @@ mean(getDepth(ad))
 plotAlignDepth(ad)
 
 
-reader <- bamReader("cp_350PE_01Err_bwa_sorted.bam", idx=TRUE) # read in the sorted bam file from bowtie2
-getRefData(reader)  # gives the name of the sequence in the reference?
+reader2 <- bamReader("cp_350PE_01Err_bwa_sorted.bam", idx=TRUE) # read in the sorted bam file from bowtie2
+getRefData(reader2)  # gives the name of the sequence in the reference?
 
-align <- getNextAlign(reader)
-failedQC(align) # did the thing it aligned to fail quality check? false = no
-pcrORopt_duplicate(align) #
-name(align)
-flag(align)
-refID(align)
-position(align)
-mapQuality(align)
-cigarData(align)
-nCigar(align)
-mateRefID(align)
+align2 <- getNextAlign(reader2)
+failedQC(align2) # did the thing it aligned to fail quality check? false = no
+pcrORopt_duplicate(align2) #
+name(align2)
+flag(align2)
+refID(align2)
+position(align2)
+mapQuality(align2)
+cigarData(align2)
+nCigar(align2)
+mateRefID(align2)
 
-count <- bamCountAll(reader, verbose=TRUE)
-count
+count2 <- bamCountAll(reader2, verbose=TRUE)
+count2
 
-coords <- c(0,0,15000)
-range <- bamRange(reader, coords)
-countNucs(range)
-ncs <- nucStats(reader)
+coords2 <- c(0,0,15000)
+range2 <- bamRange(reader2, coords2)
+countNucs(range2)
+ncs <- nucStats(reader2)
 ncs
 
 
-xlim <- c(1,100000)
-coords <- c(0,xlim[1], xlim[2])
-range <- bamRange(reader, coords)
-ad <- alignDepth(range)
-mean(getDepth(ad))
-plotAlignDepth(ad)
+xlim2 <- c(1,100000)
+coords2 <- c(0,xlim2[1], xlim2[2])
+range2 <- bamRange(reader2, coords2)
+ad2 <- alignDepth(range2)
+mean(getDepth(ad2))
+plotAlignDepth(ad2)
 
